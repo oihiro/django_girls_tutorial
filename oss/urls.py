fdb7bf8003679327303cc6902709afbe78137ab8
Oi Hirokazu <oi.hirokazu@gmail.com>
2019-08-16 15:14:08 +0900

    Added article_detail

--- oss/urls.py
+++ oss/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.article_list, name='article_list')
+    path('', views.article_list, name='article_list'),
+    path('article/<int:pk>/', views.article_detail, name='article_detail')
 ]
--- oss/views.py
+++ oss/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 # Create your views here.
 def article_list(request):
     articles = Article.objects.all().order_by('-updated_date')
     return render(request, 'oss/article_list.html', {'articles': articles})
+
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, 'oss/article_detail.html', {'article': article})
