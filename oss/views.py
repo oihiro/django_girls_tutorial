9c079759e7bcf99a6e117aca3b14c669a651d537
Oi Hirokazu <oi.hirokazu@gmail.com>
2019-08-16 12:25:37 +0900

    Modified templates

--- oss/views.py
+++ oss/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+def article_list(request):
+    articles = Article.objects.all().order_by('-updated_date')
+    return render(request, 'oss/article_list.html', {'articles': articles})
