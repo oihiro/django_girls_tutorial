1ee8e751a26f153a57767644e86e42322437666e
Oi Hirokazu <oi.hirokazu@gmail.com>
2019-08-18 17:15:40 +0900

    Django girls tutorial finished

--- oss/urls.py
+++ oss/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.article_list, name='article_list'),
-    path('article/<int:pk>/', views.article_detail, name='article_detail')
+    path('article/<int:pk>/', views.article_detail, name='article_detail'),
+    path('article/new/', views.article_new, name='article_new'),
+    path('article/<int:pk>/edit/', views.article_edit, name='article_edit'),
 ]
--- oss/views.py
+++ oss/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 def article_list(request):
@@ -9,3 +11,32 @@ def article_list(request):
 def article_detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
     return render(request, 'oss/article_detail.html', {'article': article})
+
+def article_new(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.created_date = timezone.now()
+            article.updated_date = timezone.now()
+            article.save()
+            return redirect('article_detail', pk=article.pk)
+    else:
+        form = ArticleForm()
+    return render(request, 'oss/article_new.html', {'form': form})
+
+def article_edit(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.created_date = timezone.now()
+            article.updated_date = timezone.now()
+            article.save()
+            return redirect('article_detail', pk=article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    return render(request, 'oss/article_new.html', {'form': form})
